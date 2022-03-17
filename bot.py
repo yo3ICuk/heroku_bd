0a6a68f9b66dcd1c334ef1f73623ae33f5c59c1e
unknown <egorhoom@gmail.com>
2022-03-17 22:27:32 +0300

    Add databse connection

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from config import DB_URI
 
 bot = telebot.TeleBot("5061949277:AAFhKaHQzLrrE0SKuARQtEHjFVXmr0y7EDw")
 
-db_connection = psycopg2.connect(DB_URI, sslmode="require")
+db_connection = psycopg2.connect("postgres://lkfhzccwevilyp:ab9ef31b0df7cc1c2af8dcc27e9068630f41b412a10452181ce5df168c9007ff@ec2-34-247-151-118.eu-west-1.compute.amazonaws.com:5432/d7ksen328pgoro", sslmode="require")
 db_object = db_connection.cursor()
 
 
